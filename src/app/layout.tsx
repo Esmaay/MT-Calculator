fbe7cfed297e4d75f46ad5ccc3b89595d4a53c16
Esmaybe <esmaybe@revenity.net>
2024-02-01 14:41:10 +0100

    aaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 
 import { Inter } from 'next/font/google';
 
-import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Analytics />
+        <GoogleAnalytics gaId="G-CNXQEXMR4T" />
         <SpeedInsights />
       </body>
     </html>
